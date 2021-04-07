e433e168992197ac53a93afaec8dc90f7e3bffd0
Rahul Veer Singh <singh.rahul220499@gmail.com>
2021-04-07 10:12:42 +0530

    assessment

---
+++ Testing/src/com/dxc/pack1/Test.java
@@ -0,0 +1,12 @@
+package com.dxc.pack1;
+import java.util.*;
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer i = 34;
+		 String str = (i<21)?"jan":(i<56)?"feb":"march";
+		 System.out.println(str);
+
+}
+}
