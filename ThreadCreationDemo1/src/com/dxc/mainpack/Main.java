ee9f8f1f319d1d7537ae870db92b117d2cec4a54
Rahul Veer Singh <singh.rahul220499@gmail.com>
2021-04-08 12:56:34 +0530

    MultiThreading

--- ThreadCreationDemo1/src/com/dxc/mainpack/Main.java
+++ ThreadCreationDemo1/src/com/dxc/mainpack/Main.java
@@ -7,13 +7,14 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ThreadClass1 tob1 = new ThreadClass1();
-		Thread t1 = new Thread(tob1); //new statement of thread
-		
-		t1.setName("Thread1");
-		
+		Thread t1 = new Thread(tob1); //new statement of thread		
+		t1.setName("Thread1");		
 		//to go to runnable state
 		t1.start();
 		//to run the thread, run method is going to be executed
 		
+		Thread t2 = new Thread(tob1);
+		t2.setName("Thread2");
+		t2.start();
 	}
 }
--- ThreadCreationDemo1/src/com/dxc/tpack/ThreadClass1.java
+++ ThreadCreationDemo1/src/com/dxc/tpack/ThreadClass1.java
@@ -5,11 +5,11 @@ public class ThreadClass1 implements Runnable {
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		for(int i=0;i<20;i++)
+		for(int i=0;i<5;i++)
 		{
 			System.out.println(i+" "+Thread.currentThread().getName());
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(3000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				System.out.println(e.getMessage());
