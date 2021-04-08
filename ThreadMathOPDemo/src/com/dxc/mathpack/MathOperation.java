8553f4cc57a8324b2f110b877dc3af0a7f15c06e
Rahul Veer Singh <singh.rahul220499@gmail.com>
2021-04-08 17:33:42 +0530

    MultiThreading

--- ThreadMathOPDemo/src/com/dxc/mathpack/MathOperation.java
+++ ThreadMathOPDemo/src/com/dxc/mathpack/MathOperation.java
@@ -9,6 +9,13 @@ public class MathOperation {
 	}
 	public synchronized void sub(int num)
 	{
+		if(i<=0)
+			try {
+				this.wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		i-=num;
 		System.out.println("Value of i after sub: "+i);
 	}
